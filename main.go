aa7b17b1516563c1c4d03d6e49472d3e99476287
Strebzilla <matt.streb@gmail.com>
2026-03-12 19:15:58 -0400

    Added metrics middleware

--- main.go
+++ main.go
@@ -1,14 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
-func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	const healthCheckBody = "OK"
+func (cfg * apiConfig) getMetricsHandler(w http.ResponseWriter, r *http.Request) {
+	responseText := fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())
 	
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(responseText))
+}
+
+func (cfg * apiConfig) resetMetricsHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {	
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
@@ -18,9 +41,15 @@ func main() {
 	const filepathroot = "./app"
 	const port = "8080"
 
+	apiConfig := apiConfig{}
+
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathroot))))
+	appHandler := http.StripPrefix("/app", http.FileServer(http.Dir(filepathroot)))
+
+	mux.Handle("/app/", apiConfig.middlewareMetricsInc(appHandler))
 	mux.HandleFunc("/healthz", healthCheckHandler)
+	mux.HandleFunc("/metrics", apiConfig.getMetricsHandler)
+	mux.HandleFunc("/reset", apiConfig.resetMetricsHandler)
 
 	srv := &http.Server{
 		Addr: ":8080",
