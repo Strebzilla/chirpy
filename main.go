aeb0b1fe79fff40be1725704cebeb3881b0dfd4f
Strebzilla <matt.streb@gmail.com>
2026-03-12 17:11:05 -0400

    Added a health check handler

--- main.go
+++ main.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
-func main() {
-	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
-	mux.Handle("/assets/*", http.FileServer(http.Dir("./assets")))
 
-	server := &http.Server{
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	const healthCheckBody = "OK"
+	
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func main() {
+	const filepathroot = "./app"
+	const port = "8080"
+
+	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathroot))))
+	mux.HandleFunc("/healthz", healthCheckHandler)
+
+	srv := &http.Server{
 		Addr: ":8080",
 		Handler: mux,
 	}
-	server.ListenAndServe()
+	
+	log.Printf("Serving files from %s on port: %s\n", filepathroot, port)
+	log.Fatal(srv.ListenAndServe())
 }
