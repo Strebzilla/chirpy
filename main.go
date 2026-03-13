f951fdc8deac60c427f031ed7e17565fc5860f55
Strebzilla <matt.streb@gmail.com>
2026-03-12 20:49:39 -0400

    Learning about handlers

--- main.go
+++ main.go
@@ -11,15 +11,22 @@ type apiConfig struct {
 	fileserverHits atomic.Int32
 }
 
-func (cfg * apiConfig) getMetricsHandler(w http.ResponseWriter, r *http.Request) {
-	responseText := fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())
-	
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+func (cfg *apiConfig) getMetricsHandler(w http.ResponseWriter, r *http.Request) {
+	responseText := fmt.Sprintf(`
+	<html>
+		<body>
+			<h1>Welcome, Chirpy Admin</h1>
+			<p>Chirpy has been visited %d times!</p>
+		</body>
+	</html>
+	`, cfg.fileserverHits.Load())
+
+	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(responseText))
 }
 
-func (cfg * apiConfig) resetMetricsHandler(w http.ResponseWriter, r *http.Request) {
+func (cfg *apiConfig) resetMetricsHandler(w http.ResponseWriter, r *http.Request) {
 	cfg.fileserverHits.Store(0)
 	w.WriteHeader(http.StatusOK)
 }
@@ -31,7 +38,7 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	})
 }
 
-func healthCheckHandler(w http.ResponseWriter, r *http.Request) {	
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
@@ -47,15 +54,15 @@ func main() {
 	appHandler := http.StripPrefix("/app", http.FileServer(http.Dir(filepathroot)))
 
 	mux.Handle("/app/", apiConfig.middlewareMetricsInc(appHandler))
-	mux.HandleFunc("/healthz", healthCheckHandler)
-	mux.HandleFunc("/metrics", apiConfig.getMetricsHandler)
-	mux.HandleFunc("/reset", apiConfig.resetMetricsHandler)
+	mux.HandleFunc("GET /admin/metrics", apiConfig.getMetricsHandler)
+	mux.HandleFunc("POST /admin/reset", apiConfig.resetMetricsHandler)
+	mux.HandleFunc("GET /api/healthz", healthCheckHandler)
 
 	srv := &http.Server{
-		Addr: ":8080",
+		Addr:    ":8080",
 		Handler: mux,
 	}
-	
+
 	log.Printf("Serving files from %s on port: %s\n", filepathroot, port)
 	log.Fatal(srv.ListenAndServe())
 }
